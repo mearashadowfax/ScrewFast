5b71fa87dfb0081996fc564f1fedc5e92e949bc8
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-11 22:47:39 +0400

    Update dependencies and tailwind configuration
    The package.json and package-lock.json files have been updated to include new development tools: Prettier, its Astro and TailwindCSS plugins, and a package called preline. The tailwind.config.mjs file has also been updated to reference preline and its plugin as well as to enable Tailwind's dark mode feature.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/preline/preline.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [require("preline/plugin")],
+};
