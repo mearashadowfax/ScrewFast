9b5d157267bb5d38da8ce802da1576873ae3c2b9
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-12 17:17:04 +0400

    Update website configuration and improve UI
    The website's configuration is updated with new site URL and vercelStatic adapter modified with image configuration. The UI is also enhanced, with color and aspect ratio adjustments in PricingSection and TabContent. Moreover, image sizes in ClientsSection are regularized.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,13 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://screwfast.uk',
+  site: 'https://screw-fast.vercel.app',
   integrations: [tailwind(), sitemap()],
   output: 'static',
-  adapter: vercelStatic(),
+  adapter: vercelStatic({
+    imagesConfig: {
+      sizes: [400, 600, 800, 1000, 1200, 1600, 2000],
+    },
+    imageService: true,
+  }),
 });
