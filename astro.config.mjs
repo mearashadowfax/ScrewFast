2acfdaa84aee7dcf17834e545dc3f70701562e1d
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-12 17:36:50 +0400

    Simplify image configuration and update button text sizing
    The image handling configuration within astro.config.mjs has been simplified by removing explicit sizing and format details. Concurrently, button text sizing for both PrimaryCTA and SecondaryCTA components has been updated to scale responsively on 2xl views, ensuring better user interface on larger screens.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,5 @@ export default defineConfig({
   site: 'https://screw-fast.vercel.app',
   integrations: [tailwind(), sitemap()],
   output: 'static',
-  adapter: vercelStatic({
-    imagesConfig: {
-      sizes: [400, 600, 800, 1000, 1200, 1600, 2000],
-      domains: [],
-      formats: ["image/avif", "image/webp"],
-    },
-    imageService: true,
-  }),
+  adapter: vercelStatic(),
 });
