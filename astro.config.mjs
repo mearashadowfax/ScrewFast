3f6efbc54f751da09c2d4aaf0412f838c14815ac
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-12 07:36:55 +0400

    Update dependencies and adjust styles
    Added new dependencies to the Astro and npm configuration files which includes sitemap, vercel, and other packages. Removed the HSThemeAppearance script in ThemeIcon.astro and the favicon.svg file. Made minor adjustments to the class attributes of some components in HeroSection.astro for styling purposes. These changes aim to enhance website performance and user experience. Furthermore, the addition of the sitemap package will help improve website SEO.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from '@astrojs/vercel/static';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://screwfast.uk',
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
+  adapter: vercelStatic(),
 });
