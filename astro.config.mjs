e3888b18cbaa64329edda87709fcf721361464cb
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-19 08:58:15 +0400

    Add HTML minification process and update dependencies
    A new script for HTML minification has been added, which is run after the build process. Several dependencies were updated, including the Astro framework and its related plugins. The 'astro-critters' package and its usage in the Astro configuration were removed. A minor animation timing change was also made in the [...slug].astro file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
-import critters from "astro-critters";
 
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://screw-fast.vercel.app',
-  integrations: [tailwind(), sitemap(), critters(), compressor({ gzip: false, brotli: true })],
+  integrations: [tailwind(), sitemap(), compressor({ gzip: false, brotli: true })],
   output: 'static',
   adapter: vercelStatic()
 });
---
+++ process-html.mjs
@@ -0,0 +1,22 @@
+import fs from 'node:fs/promises'
+import { globby } from 'globby'
+import { minify } from 'html-minifier'
+
+// Get all HTML files from the output directory
+const path = './.vercel/output/static'
+const files = await globby(`${path}/**/*.html`)
+
+await Promise.all(
+    files.map(async (file) => {
+   	 console.log('Processing file:', file)
+   	 let html = await fs.readFile(file, 'utf-8')
+
+   	 // Minify the HTML
+   	 html = minify(html, {
+   		 removeComments: true,
+   		 preserveLineBreaks: true,
+   		 collapseWhitespace: true
+   	 })
+   	 await fs.writeFile(file, html)
+    })
+)
