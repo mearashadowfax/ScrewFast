c547ce4d7dfd413f288e8b0599caa6c99f759390
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-02-19 08:00:15 +0400

    Add 'astro-compressor' and 'astro-critters' to astro config and packages
    This commit introduces 'astro-compressor' and 'astro-critters' to the project's astro configuration file and package files. This is reflected in both the regular package file and the package lock file. The compressor integration has gzip disabled and brotli enabled in the astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
+import critters from "astro-critters";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://screw-fast.vercel.app',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), critters(), compressor({ gzip: false, brotli: true })],
   output: 'static',
-  adapter: vercelStatic(),
+  adapter: vercelStatic()
 });
