e0cec536e704048cba43eeb5fc51edd907d04b02
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-03-17 17:25:44 +0400

    Update astro.config.mjs and add astro-critters package
    Added the astro-critters package to improve critical CSS inlining and prioritization. This update involves changes to both the astro.config.mjs to import the package and set it up for integration, and package.json along with package-lock.json to add the package.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,20 @@ import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: 'https://screwfast.uk',
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
   prefetch: true,
-  integrations: [tailwind(), sitemap(), compressor({ gzip: false, brotli: true })],
+  integrations: [tailwind(), sitemap(), compressor({
+    gzip: false,
+    brotli: true
+  }), critters()],
   output: 'static',
   experimental: {
     clientPrerender: true,
