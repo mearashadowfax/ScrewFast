9c3592adaf54559c1bc22a39de44a62b8fc16c92
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-03-20 07:54:52 +0400

    Remove Starlight Tailwind plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import starlightPlugin from '@astrojs/starlight-tailwind';
+
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -10,7 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    starlightPlugin(),
     require("tailwindcss/nesting"),
     require("preline/plugin"),
     require("@tailwindcss/forms")
