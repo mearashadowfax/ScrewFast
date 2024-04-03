d795a4a57826a936bf6222815e3fc1bd6614209c
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-04-03 15:45:19 +0400

    Change locale codes in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,8 +29,8 @@ export default defineConfig({
       i18n: {
         defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
         locales: {
-          en: "en-US", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr-CA",
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          fr: "fr",
         },
       },
     }),
