ff3f4fad3f76feed8a3a5c84d51eb43d16a7ea42
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-04-04 22:45:05 +0400

    feat: Create separate branch for single language version

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,28 +12,10 @@ export default defineConfig({
   image: {
     domains: ["images.unsplash.com"],
   },
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"],
-    fallback: {
-      fr: "en",
-    },
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
   prefetch: true,
   integrations: [
     tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
-        locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
-        },
-      },
-    }),
+    sitemap(),
     starlight({
       title: "ScrewFast Docs",
       defaultLocale: "root",
