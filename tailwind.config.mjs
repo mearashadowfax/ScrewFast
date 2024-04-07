c92a0670400a2901e1d52c66ad360ff73befbf17
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-04-07 18:23:16 +0400

    Replace color hex codes with Tailwind color palette
    Replaced all custom color hex codes used in various component classes with their corresponding color names from the expanded Tailwind color palette. This ensures consistent styling across components and enhances readability and maintainability of the code.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,66 @@ export default {
   ],
   darkMode: "class",
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#ffffff",
+      gray: {
+        100: "#f3f4f6",
+        300: "#d1d5db",
+        500: "#6b7280",
+        600: "#4b5563",
+        800: "#1f2937",
+      },
+      indigo: {
+        200: "#c7d2fe",
+        300: "#a5b4fc",
+      },
+      neutral: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#e5e5e5",
+        300: "#d4d4d4",
+        400: "#a3a3a3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+      yellow: {
+        50: "#fefce8",
+        100: "#fef9c3",
+        400: "#facc15",
+        500: "#eab308",
+      },
+      orange: {
+        100: "#ffedd5",
+        200: "#fed7aa",
+        300: "#fb713b",
+        400: "#fa5a15",
+        500: "#e14d0b",
+        600: "#ea580c",
+      },
+      red: {
+        400: "#f87171",
+        500: "#ef4444",
+      },
+      zinc: {
+        200: "#e4e4e7",
+        500: "#71717a",
+        600: "#52525b",
+        700: "#3f3f46",
+        800: "#27272a",
+        900: "#18181b",
+      },
+    },
     extend: {},
   },
   plugins: [
     require("tailwindcss/nesting"),
     require("preline/plugin"),
-    require("@tailwindcss/forms")
+    require("@tailwindcss/forms"),
   ],
 };
