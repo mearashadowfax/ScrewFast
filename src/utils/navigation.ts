dddc7940b4d5ca2b3fd8990bcd0fd65f795b9e8e
Vasile Pop <vasile.pop@gmail.com>
2024-04-26 22:20:01 +0300

    adding technologies page

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,7 +1,7 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Tehnologii", url: "/products" },
+  { name: "Tehnologii", url: "/technologies" },
   { name: "Servicii", url: "/services" },
   { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
