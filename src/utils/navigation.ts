513e17238fcf1062ea60d4ea11fc5662917176cb
Vasile Pop <vasile.pop@gmail.com>
2024-05-04 22:45:44 +0300

    removed unused images

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,32 +1,10 @@
-// An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
   { name: "Tehnologii", url: "/technologies" },
   { name: "Servicii", url: "/services" },
-  { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
-// An array of links for footer
-const footerLinks = [
-  {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
-    ],
-  },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
-];
-// An object of links for social icons
+
 const socialLinks = {
   facebook: "https://www.facebook.com/healthacademycluj/",
   x: "https://twitter.com/",
@@ -35,6 +13,5 @@ const socialLinks = {
 
 export default {
   navBarLinks,
-  footerLinks,
   socialLinks,
 };
