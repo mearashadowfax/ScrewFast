33d69c0be84d6cc9f628f5de8971e68941bb8fe9
Vasile Pop <vasile.pop@gmail.com>
2024-05-04 23:08:43 +0300

    udpate vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,35 @@ import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://healthacademy.ro",
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
   i18n: {
     defaultLocale: "ro",
     locales: ["ro"],
     routing: {
-      prefixDefaultLocale: false,
-    },
+      prefixDefaultLocale: false
+    }
   },
   prefetch: true,
-  integrations: [
-    tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: "ro", 
-      },
-    }),
-    compressor({
-      gzip: false,
-      brotli: true,
-    }),
-  ],
+  integrations: [tailwind(), sitemap({
+    i18n: {
+      defaultLocale: "ro"
+    }
+  }), compressor({
+    gzip: false,
+    brotli: true
+  })],
   output: "static",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true,
+    directRenderScript: true
   },
-  adapter: vercelStatic(),
+  adapter: vercel()
 });
