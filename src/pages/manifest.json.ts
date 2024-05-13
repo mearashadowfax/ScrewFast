3c663040cdacb342890c5235a3c27ee61811b682
Guido X Jansen <x@gui.do>
2024-05-14 01:41:09 +0200

    Template name replacements

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -44,8 +44,8 @@ export const GET: APIRoute = async () => {
   );
 
   const manifest = {
-    short_name: "ScrewFast",
-    name: "ScrewFast",
+    short_name: "CommerceQuest",
+    name: "CommerceQuest",
     icons,
     display: "minimal-ui",
     id: "/",
