19a4344c2623e1a666c52fb276567c58d3a7a246
Guido X Jansen <x@gui.do>
2024-05-13 17:35:23 +0200

    Update astro.config.mjs
    Updated the site URL
    
    Signed-off-by: Guido X Jansen <x@gui.do>

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://screwfast.uk",
+  site: "https://spryker-community.github.io",
   image: {
     domains: ["images.unsplash.com"],
   },
