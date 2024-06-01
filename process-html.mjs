d38f25b50a3f593d41a12fcb85d864916b2d77a8
Guido X Jansen <x@gui.do>
2024-06-01 23:34:59 +0200

    Removed Vercel stuff

--- process-html.mjs
+++ process-html.mjs
@@ -3,7 +3,6 @@ import { globby } from 'globby'
 import { minify } from 'html-minifier-terser'
 
 // Get all HTML files from the output directory
-const path = './.vercel/output/static'
 const files = await globby(`${path}/**/*.html`)
 
 await Promise.all(
