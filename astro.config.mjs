df9b56efd85a85fa1ba1b69fb796fd00dbfae609
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-06-22 17:47:58 +0400

    Add ThemeSelect component with theme toggle functionality to the Starlight's Navbar
    A new ThemeSelect.astro file was added to let users toggle between light and dark themes in Starlight Docs. This component replaces the default ThemeProvider.astro component

--- astro.config.mjs
+++ astro.config.mjs
@@ -93,6 +93,7 @@ export default defineConfig({
       components: {
         SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
         Head: "./src/components/ui/starlight/Head.astro",
+        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
       },
       head: [
         {
