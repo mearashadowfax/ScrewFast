020679f26c653188cafbea90d38709d5586b7a78
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-09-07 21:51:07 +0400

    Ensure correct URL handling for product page translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
@@ -123,5 +122,4 @@ export default defineConfig({
     clientPrerender: true,
     directRenderScript: true,
   },
-  adapter: vercelStatic(),
 });
