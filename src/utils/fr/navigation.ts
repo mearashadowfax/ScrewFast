863e00bf68e3e5bf931f41d5c5733d233064e80f
SrRickGrimes <81363560+SrRickGrimes@users.noreply.github.com>
2024-10-20 08:06:13 +0900

    updated landing page

--- src/utils/fr/navigation.ts
+++ src/utils/fr/navigation.ts
@@ -1,10 +1,7 @@
 
 const navBarLinks = [
   { name: "Accueil", url: "/fr" },
-  { name: "Produits", url: "/fr/products" },
-  { name: "Services", url: "/fr/services" },
   { name: "Blog", url: "/fr/blog" },
-  { name: "Contact", url: "/fr/contact" },
 ];
 
 const footerLinks = [
--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,10 +1,7 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
   { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
