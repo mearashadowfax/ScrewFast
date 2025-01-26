57b8919cc680d87134068735365b1b97c9544ad2
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2025-01-26 15:20:08 +0400

    refactor: update yellow color configuration to reference tailwind's colors.yellow

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
+import colors from "tailwindcss/colors";
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -14,20 +14,13 @@ export default {
       white: "#ffffff",
       gray: colors.gray,
       indigo: colors.indigo,
-      neutral: colors.neutral,  // Used mainly for text color
-      yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
-      }, // Accent colors, used mainly for star color, heading and buttons
+      neutral: colors.neutral, // Used mainly for text color
+      yellow: colors.yellow, // Accent colors, used mainly for star color, heading and buttons
       orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
+        ...colors.orange,
         300: "#fb713b",
         400: "#fa5a15",
         500: "#e14d0b",
-        600: "#ea580c",
       }, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
