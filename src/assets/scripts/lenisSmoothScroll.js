3d9336bdfbe61a1d8b680e61500668871c8a38ed
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2025-03-03 22:19:45 +0400

    Update Lenis initialization for smooth scrolling
    Replaced manual requestAnimationFrame setup with `autoRaf` option in Lenis initialization. This simplifies the code and leverages Lenis's built-in functionality for smoother and more efficient scrolling.

--- src/assets/scripts/lenisSmoothScroll.js
+++ src/assets/scripts/lenisSmoothScroll.js
@@ -3,11 +3,7 @@ import "@styles/lenis.css";
 import Lenis from "lenis";
 
 // Script to handle Lenis library settings for smooth scrolling
-const lenis = new Lenis();
-
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+// https://github.com/darkroomengineering/lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
