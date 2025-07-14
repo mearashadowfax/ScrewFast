0d20e951c749272305194bd9fd738ce7dec2b1d4
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2025-07-15 01:39:46 +0400

    feat: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,6 @@ export default defineConfig({
     sitemap(),
     starlight({
       title: "ScrewFast Docs",
-      defaultLocale: "root",
       locales: {
         root: {
           label: "English",
